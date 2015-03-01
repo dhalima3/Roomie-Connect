f3aebba9f5d45348b5d33b359fe39ce9b51eb8b9
kylej1994 <kylejablon@wirelessprvnat-172-17-166-174.near.illinois.edu>
2015-03-01 02:05:50 -0600

    Finished Communicate

--- models/Communication.js
+++ models/Communication.js
@@ -1,65 +1,86 @@
 var mongoose = require('mongoose');
 var Chore = require('../models/Chore');
+var User = require('../models/User');
 var secrets = require('../config/secrets');
-var Mailjet = require('mailjet-sendemail')
+var Mailjet = require('mailjet-sendemail');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var mainLoop = ({
-  //set up main communications loop
+// var mainLoop = function(){
+//set up main communications loop
+  mongoose.connect('mongodb://localhost/test');
   var db = mongoose.connection;
-  //fetch all of todays chores
-  var allChores = Chore.where('daysInBetween').equals(0);
 
-  var communicate = function(Chore)({
+setInterval(function(){
+}, 1000);
+  var communicate = function(Chore){
+      if (Chore.sent === true)
+        return;
       //find user who must do the chore
       var userToFind = Chore.lastPerson;
-      var currUser = User.findOne({'name.profile': userToFind}, function(err, User){
+
+      User.findOne({'profile.name': Chore.lastPerson}, function(err, currUser){
         if (err) return handleError(err);
-      });
 
       //Twilio set up. Send SMS text.
       var client = require('twilio')(secrets.twilio.sid,
         secrets.twilio.token);
       client.messages.create({
-        to: currUser.profile.phonenumber,
+        to: currUser.profile.phoneNumber,
       	from: secrets.twilio.phone,
-        body: "You have a chore today! Chore name:" //+ Chore.name
+        body: "You have a chore today! Chore name: " + Chore.name
       }, function(err, message) {
       	console.log(message.sid);
       });
-
 
       //Mailjet set up
       var mailjetAPI = secrets.mailjet.apiKey;
       var mailjetSecret = secrets.mailjet.secret;
       var mailjet = new Mailjet(mailjetAPI, mailjetSecret);
-      from = "jablonk@uchicago.edu"
+      from = "jablonk@uchicago.edu" //example domain e-mail
       to = currUser.email
       subject = "Your Chores"
       type = "html"
       basicContent = "This is an automated e-mail to remind you of your roommate duties."
       choreContent = Chore.name + Chore.descrip;
-      mailjet.sendContent(from, to, subject, type, content);
+      mailjet.sendContent(from, to, subject, type, basicContent + choreContent);
 
-      //Interactive Intelligence set up
+    //Interactive Intelligence set up
 
       var interactInt = secrets.interactInt;
-      var interactapp = interactInt.appID;
-      var interactkey = interactInt.apiKey;
+      //var interactapp = interactInt.appID;
+      var interactkey = "ee81bbe89124b6ea14adcff504"; //interactInt.apiKey;
 
       var http = new XMLHttpRequest();
-      var url = "http://hackathonapi.inin.com/api" + interactapp +"/call/callandplaytts";
-      var ttsmessage = "You have a chore! You must" + currentChore.name + currentChore.descrip;
-      var data= {"number":currUser.profile.phonenumber, "message":ttsmessage};
+      var url = "http://hackathonapi.inin.com/api/442689/call/callandplaytts";
+      var ttsmessage = "hello"; //"You have a chore! You must  " + Chore.name;
+      var data= {"number": "8583535978", "message":ttsmessage};
       http.open("POST", url, true);
 
       http.setRequestHeader("Content-type", "application/json");
-      http.setRequestHeader("Api-Key", apikey);
-      http.send(JSON.stringify(data))
-
+      http.setRequestHeader("Api-Key", interactkey);
+      http.send(JSON.stringify(data));
+      console.log(JSON.stringify(data));
+      console.log("I have sent all the data");
+      //Change status to reflect that you already sent a notification
+      Chore.sent = true;
       return;
-  });
-
-  // while(true){
+    });
+  };
+  // var userKyle = new User({'profile': {'name': 'Kyle', 'phoneNumber': '8583535978'},
+  //  'email': 'kylejablon@uchicago.edu'});
+  // userKyle.save();
+  // var userSam = new User({'profile': {'name': 'Sam', 'phoneNumber': '3037209931'},
+  //  'email': 'samjbaugh@uchicago.edu'});
+  // userSam.save();
   //
-  // }
-});
+  // var newChore = new Chore({'name': 'Laundry', 'lastPerson': 'Kyle'});
+  // newChore.save();
+  //
+  // var choreForSam = new Chore({'name': 'Oppress the masses', 'lastPerson': 'Sam'})
+  // choreForSam.save();
+  //
+  //
+  //
+  // communicate(newChore);
+
+// };
